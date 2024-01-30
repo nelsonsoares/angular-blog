3e6e8083ef3bbfb5be4d695e0d2e7d4cf896f697
nelsonsoares <nelson.nsf40@gmail.com>
2024-01-30 01:58:41 -0300

    first commit: menu-title and big-card done

---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { MenuTitleComponent } from '../../components/menu-title/menu-title.component';
+import { BigCardComponent } from '../../components/big-card/big-card.component';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [MenuTitleComponent, BigCardComponent],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss'
+})
+export class HomeComponent {
+
+}
