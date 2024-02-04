4f4b1928a1f0271a07cfd566d697d02a52dc659f
nelsonsoares <nelson.nsf40@gmail.com>
2024-02-04 17:59:17 -0300

    feat: creation the properties in the components

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-big-card',
   standalone: true,
   imports: [],
   templateUrl: './big-card.component.html',
-  styleUrl: './big-card.component.scss'
+  styleUrl: './big-card.component.scss',
 })
 export class BigCardComponent {
-
+  @Input() bigCardPhoto: string = '';
+  @Input() bigCardPhotoAlt: string = '';
+  @Input() bigCardTitle: string = '';
+  @Input() bigCardContent: string = '';
 }
